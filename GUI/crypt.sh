1052f96934e7641c9fac327d240673a6ba1e0334
nrssi <84055061+nrssi@users.noreply.github.com>
2021-06-18 14:19:07 +0530

    files related to tthe project
    this is the first commit and no bugs have been found yet

---
+++ GUI/crypt.sh
@@ -0,0 +1,79 @@
+#! /bin/env bash
+#
+# ╔╗╔═╗
+# ║║║╔╝
+# ║╚╝╝
+# ║╔╗║
+# ║║║╚╗   Kusumavathi (4SU19CS0)
+# ╚╝╚═╝
+#
+# ╔═╗ ╔╗
+# ║║╚╗║║
+# ║╔╗╚╝║
+# ║║╚╗║║
+# ║║ ║║║  N R Shyamsundar Iyanger (4SU19CS057)
+# ╚╝ ╚═╝
+#
+# ╔╗
+# ║║
+# ║║
+# ║║ ╔╗
+# ║╚═╝║   Laxmi Magadum (4SU19CS0)
+# ╚═══╝
+#
+# ╔═╗ ╔╗
+# ║║╚╗║║
+# ║╔╗╚╝║
+# ║║╚╗║║
+# ║║ ║║║  Nachiketh (4SU19CS058)
+# ╚╝ ╚═╝
+#
+      
+# this script enables user to interactives encrypt or decrypt a image using rofi as the gui driver utility
+# although rofi is not a pure gui utility, it neatly fits our need so we used it for our convinience
+
+#function retrieves user input and then feeds the calling variable with it
+user_input() {
+  killall rofi &>/dev/null; # killing all the rofi instances to avoid colliding
+  #call to rofi ( a gui menu widget )
+  rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "$1"\
+		-theme ./confirm.rasi
+}
+
+# Message
+# Provides the given message in a nice little box to the user
+msg() {
+	rofi -theme "./message.rasi" -e "$1"
+}
+#
+# Driver function
+
+main(){
+	ans=$(user_input "Select 1.Encrypt 2.Decrypt ->" &)
+  if [[ $ans == "1" || $ans == "1 Encrypt" || $ans == "encrypt" || $ans == "Encrypt" ]]; then
+    source_path=$(user_input "Enter input file path->");
+    destination_path=$(user_input "Enter output file path->");
+    message=$(user_input "Enter the message to encode ->");
+    res=$(./encode.py "$source_path" "$PWD/$destination_path" "$message");
+    pwait ./encode.py ;
+    if [[ $res == "" ]]
+    then 
+      res="Failed to encode, Please check path names"
+    fi
+    msg "$res";
+  elif [[ $ans == "2" || $ans == "2 Decrypt" || $ans == "decrypt" || $ans == "Decrypt" ]]; then
+    source_path=$(user_input "Enter the source path ->");
+    res=$(./decode.py $source_path);
+    if [[ $res == "" ]]
+    then 
+      res="Failed to decode, Please check path names"
+    fi
+    msg "$res";
+  else
+    msg "Available options- encode/1/ or decode/2/"
+	fi
+}
+main
